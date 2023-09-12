e091838cfeca6733bc212258c14b5a051fd599e3
Salman Rahaman Jony <jonyislamcmt@gmail.com>
2023-09-12 21:06:08 +0600

    change headerpart

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     <>
         <header className="max-w-[80%] mx-auto py-4 border-b">
           <nav className="flex justify-between items-center">
-            <a className="text-3xl font-semibold" href="#">Knowledge Cafe</a>
+            <a className="text-3xl font-semibold" href="#">Knowledge Cafe01</a>
             <img className="w-fit cursor-pointer" src="https://i.ibb.co/B2WB82d/profile.png" alt="" />
           </nav>
         </header>
